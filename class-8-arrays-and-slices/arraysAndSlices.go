6bd8903f037ffa437900d82fa0379b2bb4bfbcea
Leonardo Suave <leonardo.suave@aoatechpartners.com.br>
2025-03-17 11:39:00 -0300

    feat(INTERN-ARRAYS): more info about arrays and slices and how to working

--- class-8-arrays-and-slices/arraysAndSlices.go
+++ class-8-arrays-and-slices/arraysAndSlices.go
@@ -23,7 +23,7 @@ func main() {
 	//========
 
 	//SLICES
-	// Slices are flexibler than arrays, but they are not a array. (Its allow only one type to all slice)
+	// Slices are flexibler than arrays, but they are not a array. (Its allow only one type to all slice and slices not have size limit)
 	slice1 := []int{10, 11, 12}
 	fmt.Println(slice1)
 
@@ -36,4 +36,13 @@ func main() {
 
 	slice2 := array2[1:3]	// To take values from a array
 	fmt.Println(slice2)
+
+	// Intern Arrays (make func - reserve memory space to some thing)
+	//OBS make(type, lenght, cap) - cap is optional and to this cap is leght value
+	slice3 := make([]float32, 10, 11) // make func create a slice with 11 index and return the 10 first index (cap is optional)
+	slice3 = append(slice3, 12)
+	slice3 = append(slice3, 15)	// Pass the cap limit 
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))	
+	fmt.Println(cap(slice3))	// When pass the limit, the slice take the size (12) and doble the cap to size 24
 }
